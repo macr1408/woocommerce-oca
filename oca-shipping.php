2fa7d414ee5a6dcbb97071e1a5190f60c6ad1271
Marcos <macr1408@hotmail.com>
2020-07-24 17:23:33 -0300

    Update address detection script

--- oca-shipping.php
+++ oca-shipping.php
@@ -136,137 +136,130 @@ function woo_oca_crear_datos_oca($datos = array(), $order = '', $operativa = '')
 
 function woo_oca_get_address($order)
 {
-    if (!$order) return false;
-    if ($order->get_shipping_address_1()) {
-        $shipping_line_1 = $order->get_shipping_address_1();
-        $shipping_line_2 = $order->get_shipping_address_2();
-    } else {
-        $shipping_line_1 = $order->get_billing_address_1();
-        $shipping_line_2 = $order->get_billing_address_2();
+    if (!$order) {
+        return false;
     }
+    if ($order->get_shipping_address_1()) {
+        $shipping_line_1 = trim($order->get_shipping_address_1());
+        $shipping_line_2 = trim($order->get_shipping_address_2());
+    } else {
+        $shipping_line_1 = trim($order->get_billing_address_1());
+        $shipping_line_2 = trim($order->get_billing_address_2());
+    }
+
+    if (empty($shipping_line_2)) {
+        // Av. Mexico 430, Piso 4 B
+        preg_match('/([^,]+),(.+)/i', $shipping_line_1, $res);
+        if (!empty($res)) {
+            $shipping_line_1 = trim($res[1]);
+            $shipping_line_2 = trim($res[2]);
+        }
+    }
+
     $street_name = $street_number = $floor = $apartment = "";
     if (!empty($shipping_line_2)) {
         //there is something in the second line. Let's find out what
         $fl_apt_array = woo_oca_get_floor_and_apt($shipping_line_2);
-        $floor = $fl_apt_array[0];
-        $apartment = $fl_apt_array[1];
+        $street_number = $fl_apt_array['number'];
+        $floor = $fl_apt_array['floor'];
+        $apartment = $fl_apt_array['apartment'];
     }
 
-    //Now let's work on the first line
-    preg_match('/(^\d*[\D]*)(\d+)(.*)/i', $shipping_line_1, $res);
-    $line1 = $res;
-
-    if ((isset($line1[1]) && !empty($line1[1]) && $line1[1] !== " ") && !empty($line1)) {
-        //everything's fine. Go ahead
-        if (empty($line1[3]) || $line1[3] === " ") {
-            //the user just wrote the street name and number, as he should
-            $street_name = trim($line1[1]);
-            $street_number = trim($line1[2]);
-            unset($line1[3]);
-        } else {
-            //there is something extra in the first line. We'll save it in case it's important
-            $street_name = trim($line1[1]);
-            $street_number = trim($line1[2]);
-            $shipping_line_2 = trim($line1[3]);
-
-            if (empty($floor) && empty($apartment)) {
-                //if we don't have either the floor or the apartment, they should be in our new $shipping_line_2
-                $fl_apt_array = woo_oca_get_floor_and_apt($shipping_line_2);
-                $floor = $fl_apt_array[0];
-                $apartment = $fl_apt_array[1];
-            } elseif (empty($apartment)) {
-                //we've already have the floor. We just need the apartment
-                $apartment = trim($line1[3]);
-            } else {
-                //we've got the apartment, so let's just save the floor
-                $floor = trim($line1[3]);
-            }
-        }
-    } else {
-        //the user didn't write the street number. Maybe it's in the second line
-        //given the fact that there is no street number in the fist line, we'll asume it's just the street name
-        $street_name = $shipping_line_1;
-
-        if (!empty($floor) && !empty($apartment)) {
-            //we are in a pickle. It's a risky move, but we'll move everything one step up
-            $street_number = $floor;
-            $floor = $apartment;
-            $apartment = "";
-        } elseif (!empty($floor) && empty($apartment)) {
-            //it seems the user wrote only the street number in the second line. Let's move it up
-            $street_number = $floor;
-            $floor = "";
-        } elseif (empty($floor) && !empty($apartment)) {
-            //I don't think there's a chance of this even happening, but let's write it to be safe
-            $street_number = $apartment;
-            $apartment = "";
+    // Street number detected in second line, check only for words in first line, it should be the street name
+    if ($street_number) {
+        preg_match('/^[a-zA-Z ]+$/i', $shipping_line_1, $res);
+        if (!empty($res)) {
+            $street_name = trim($res[0]);
+            return array('street' => $street_name, 'number' => $street_number, 'floor' => $floor, 'apartment' => $apartment);
         }
     }
+
+    // Av. Mexico 430
+    preg_match('/^([a-zA-Z.#º ]+)[ ]+(\d+)$/i', $shipping_line_1, $res);
+    if (!empty($res)) {
+        $street_name = trim($res[1]);
+        $street_number = trim($res[2]);
+        return array('street' => $street_name, 'number' => $street_number, 'floor' => $floor, 'apartment' => $apartment);
+    }
+
+    // calle 27 nro. 1458
+    preg_match('/^(calle|avenida|av)[\.#º]*[ ]+([0-9]+)[ ]+(numero|altura|nro)[\.#º]*[ ]+([0-9]+)$/i', $shipping_line_1, $res);
+    if (!empty($res)) {
+        $street_name = trim($res[1] . ' ' . $res[2]);
+        $street_number = trim($res[4]);
+        return array('street' => $street_name, 'number' => $street_number, 'floor' => $floor, 'apartment' => $apartment);
+    }
+
+    // 27 nro 1458
+    preg_match('/^(\d+)[ ]+(numero|altura|nro)[\.#º]*[ ]+([0-9]+)$/i', $shipping_line_1, $res);
+    if (!empty($res)) {
+        $street_name = trim($res[1]);
+        $street_number = trim($res[3]);
+        return array('street' => $street_name, 'number' => $street_number, 'floor' => $floor, 'apartment' => $apartment);
+    }
+
     return array('street' => $street_name, 'number' => $street_number, 'floor' => $floor, 'apartment' => $apartment);
 }
 
 function woo_oca_get_floor_and_apt($fl_apt)
 {
-    //firts we'll asume the user did things right. Something like "piso 24, depto. 5h"
-    preg_match('/(piso|p|p.) ?(\w+),? ?(departamento|depto|dept|dpto|dpt|dpt.º|depto.|dept.|dpto.|dpt.|apartamento|apto|apt|apto.|apt.) ?(\w+)/i', $fl_apt, $res);
-    $line2 = $res;
+    $street_name = $street_number = $floor = $apartment = "";
 
-    if (!empty($line2)) {
-        //everything was written great. Now lets grab what matters
-        $floor = trim($line2[2]);
-        $apartment = trim($line2[4]);
-    } else {
-        //maybe the user wrote something like "depto. 5, piso 24". Let's try that
-        preg_match('/(departamento|depto|dept|dpto|dpt|dpt.º|depto.|dept.|dpto.|dpt.|apartamento|apto|apt|apto.|apt.) ?(\w+),? ?(piso|p|p.) ?(\w+)/i', $fl_apt, $res);
-        $line2 = $res;
+    // Piso 8, dpto. A
+    preg_match('/^(piso|pso|pis|p)[\.º#]*[ ]*(\w+)[, ]*(departamento|depto|dept|dep|dpto|dpt|dto|apartamento|apto|apt)[\.º#]*[ ]*(\w+)[, ]*/i', $fl_apt, $res);
+    if (!empty($res)) {
+        $floor = trim($res[2]);
+        $apartment = trim($res[4]);
+        return array('street' => $street_name, 'number' => $street_number, 'floor' => $floor, 'apartment' => $apartment);
     }
 
-    if (!empty($line2) && empty($apartment) && empty($floor)) {
-        //apparently, that was the case. Guess some people just like to make things difficult
-        $floor = trim($line2[4]);
-        $apartment = trim($line2[2]);
-    } else {
-        //something is wrong. Let's be more specific. First we'll try with only the floor
-        preg_match('/^(piso|p|p.) ?(\w+)$/i', $fl_apt, $res);
-        $line2 = $res;
+    // Piso PB
+    preg_match('/^(piso|pso|pis|p)[\.º#]*[ ]*(\w+)$/i', $fl_apt, $res);
+    if (!empty($res)) {
+        $floor = trim($res[2]);
+        return array('street' => $street_name, 'number' => $street_number, 'floor' => $floor, 'apartment' => $apartment);
     }
 
-    if (!empty($line2) && empty($floor)) {
-        //now we've got it! The user just wrote the floor number. Now lets grab what matters
-        $floor = trim($line2[2]);
-    } else {
-        //still no. Now we'll try with the apartment
-        preg_match('/^(departamento|depto|dept|dpto|dpt|dpt.º|depto.|dept.|dpto.|dpt.|apartamento|apto|apt|apto.|apt.) ?(\w+)$/i', $fl_apt, $res);
-        $line2 = $res;
+    // 1420 Piso 8, dpto. A
+    preg_match('/^([\d]+)[ ]+(piso|pso|pis|p)[\.º#]*[ ]*(\w+)[, ]*(departamento|depto|dept|dep|dpto|dpt|dto|apartamento|apto|apt)[\.º#]*[ ]*(\w+)[, ]*/i', $fl_apt, $res);
+    if (!empty($res)) {
+        $street_number = trim($res[1]);
+        $floor = trim($res[3]);
+        $apartment = trim($res[5]);
+        return array('street' => $street_name, 'number' => $street_number, 'floor' => $floor, 'apartment' => $apartment);
     }
 
-    if (!empty($line2) && empty($apartment) && empty($floor)) {
-        //success! The user just wrote the apartment information. No clue why, but who am I to judge
-        $apartment = trim($line2[2]);
-    } else {
-        //ok, weird. Now we'll try a more generic approach just in case the user missplelled something
-        preg_match('/(\d+),? [a-zA-Z.,!*]* ?([a-zA-Z0-9 ]+)/i', $fl_apt, $res);
-        $line2 = $res;
+    // 1420 Piso 8
+    preg_match('/^([\d]+)[ ]+(piso|pso|pis|p)[\.º#]*[ ]*(\w+)[, ]*/i', $fl_apt, $res);
+    if (!empty($res)) {
+        $street_number = trim($res[1]);
+        $floor = trim($res[3]);
+        return array('street' => $street_name, 'number' => $street_number, 'floor' => $floor, 'apartment' => $apartment);
     }
 
-    if (!empty($line2) && empty($floor) && empty($apartment)) {
-        //finally! The user just missplelled something. It happens to the best of us
-        $floor = trim($line2[1]);
-        $apartment = trim($line2[2]);
-    } else {
-        //last try! This one is in case the user wrote the floor and apartment together ("12C")
-        preg_match('/(\d+)(\D*)/i', $fl_apt, $res);
-        $line2 = $res;
+    // Depto. 5, piso 24
+    preg_match('/^(departamento|depto|dept|dep|dpto|dto|apartamento|apto|apt)[\.º#]*[ ]*(\w+)[, ]*(piso|pso|pis|p)[\.º#]*[ ]*(\w+)[, ]*/i', $fl_apt, $res);
+    if (!empty($res)) {
+        $floor = trim($res[4]);
+        $apartment = trim($res[2]);
+        return array('street' => $street_name, 'number' => $street_number, 'floor' => $floor, 'apartment' => $apartment);
     }
 
-    if (!empty($line2) && empty($floor) && empty($apartment)) {
-        //ok, we've got it. I was starting to panic
-        $floor = trim($line2[1]);
-        $apartment = trim($line2[2]);
-    } elseif (empty($floor) && empty($apartment)) {
-        //I give up. I can't make sense of it. We'll save it in case it's something useful 
-        $floor = $fl_apt;
+    // depto 4A
+    preg_match('/^(departamento|depto|dept|dep|dpto|dpt|dto|apartamento|apto|apt)[\.º#]*[ ]*(\w+)$/i', $fl_apt, $res);
+    if (!empty($res)) {
+        $apartment = trim($res[2]);
+        return array('street' => $street_name, 'number' => $street_number, 'floor' => $floor, 'apartment' => $apartment);
     }
 
-    return array($floor, $apartment);
+    // 2 B
+    preg_match('/^(\d+)[ ]*(\D+)$/i', $fl_apt, $res);
+    if (!empty($res)) {
+        $floor = trim($res[1]);
+        $apartment = trim($res[2]);
+        return array('street' => $street_name, 'number' => $street_number, 'floor' => $floor, 'apartment' => $apartment);
+    }
+
+    //I give up. I can't make sense of it. We'll save it in case it's something useful 
+    return array('street' => $street_name, 'number' => $street_number, 'floor' => $fl_apt, 'apartment' => $apartment);
 }
